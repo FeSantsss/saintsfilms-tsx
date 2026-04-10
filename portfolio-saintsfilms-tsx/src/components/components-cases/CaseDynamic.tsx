0290520c822e9f997e0df50d09d2d5cd02a2c1ec
Felipy <tercetime18@gmail.com>
2026-04-10 17:11:45 -0300

    exclued navigate

--- portfolio-saintsfilms-tsx/src/components/components-cases/CaseDynamic.tsx
+++ portfolio-saintsfilms-tsx/src/components/components-cases/CaseDynamic.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "../../assets/caseDynamic.css";
 
 type imagesUrl = {
